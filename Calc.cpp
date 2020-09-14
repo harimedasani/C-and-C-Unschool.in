67c4484dabf8ea8fe55411571674bcac75091509
harimedasani <71274317+harimedasani@users.noreply.github.com>
2020-09-14 20:17:52 +0530

    Add files via upload

---
+++ Calc.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the first number: ");
+	scanf("%d",&a);
+	printf("Enter the second number: ");
+	scanf("%d",&b);
+	printf("Sum of %d and %d is %d \n",a,b,(a+b));
+	printf("Difference of %d and %d is %d \n",a,b,(a-b));
+	printf("Product of %d and %d is %d \n",a,b,(a*b));
+	printf("On dividing %d by %d quotient is %d \n",a,b,(a/b));
+	printf("On dividing %d by %d remainder is %d \n",a,b,(a%b));
+}
